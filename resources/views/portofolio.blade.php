4d7b9bf5e1ce02c5c407a9a5155a9fc44a1266c8
Danang <danang@MacBook-Air-Danang.local>
2026-04-18 18:37:54 +0700

    fix: force tailwind source scan

--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Danang Portfolio</title>
 
-    @vite('resources/css/app.css')
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
 </head>
 
